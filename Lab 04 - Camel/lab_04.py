f0706a932d59b1eccb8d5ee5becb32a9acced048
Ryan <mchldude@outlook.com>
2022-01-12 10:16:36 -0500

    Work on lab 4

---
+++ Lab 04 - Camel/lab_04.py
@@ -0,0 +1,12 @@
+done = False
+
+def main():
+    print("A. Drink from your canteen")
+    print("B. Ahead moderate speed")
+    print("C. Ahead full speed")
+    print("D. Stop for the night")
+    print("E. Status check")
+    print("Q. Quit")
+
+while done==False:
+    main
