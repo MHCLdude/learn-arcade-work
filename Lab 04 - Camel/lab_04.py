bb454525ba2338d3af39be22599027a389791473
Ryan <mchldude@outlook.com>
2022-01-17 10:11:07 -0500

    Update

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -3,19 +3,20 @@ miles_traveled = 0
 drinks_in_canteen = 5
 camel_tiredness = 0
 natives_behind = -20
-
+player_choice = ""
 print("Welcome to Camel!")
 print("You have stolen a camel to make your way across the great Mobi desert.")
 print("The natives want their camel back and are chasing you down! Survive your \ndesert trek and out run the natives.\n")
 
 def main():
+    global player_choice
     print("A. Drink from your canteen")
     print("B. Ahead moderate speed")
     print("C. Ahead full speed")
     print("D. Stop for the night")
     print("E. Status check")
     print("Q. Quit")
-
+    player_choice = input("What is your choice? ").upper
 while done == False:
     main()
     player_choice = input("What is your choice? ")
@@ -23,8 +24,3 @@ while done == False:
         yn = input("Quit? y/n ")
         if yn == "y" or "Y":
             done = True
-
-    elif player_choice == "E" or "e":
-        print("Miles Traveled: " + miles_traveled)
-        print("Drinks in canteen" + drinks_in_canteen)
-        print("The natives are " + 0 - natives_behind + " miles behind you")
